08fd4e403e9ffc3a973549017dbbb6e1d9432817
KevinCz <lenguyenhoangthanh123@gmail.com>
2026-01-02 23:26:39 +0700

    now supports latest version of attribute panel

--- src/main/java/net/pixeldreamstudios/showmeyourbuild/util/ModCompat.java
+++ src/main/java/net/pixeldreamstudios/showmeyourbuild/util/ModCompat.java
@@ -5,7 +5,7 @@ import net.fabricmc.loader.api.FabricLoader;
 public class ModCompat {
     public static final boolean TRINKETS_LOADED = FabricLoader.getInstance().isModLoaded("trinkets");
     public static final boolean PUFFISH_LOADED = FabricLoader.getInstance().isModLoaded("puffish_skills");
-    public static final boolean ATTRIBUTE_PANEL_LOADED = FabricLoader.getInstance().isModLoaded("kevs-attributes-panel");
+    public static final boolean ATTRIBUTE_PANEL_LOADED = FabricLoader.getInstance().isModLoaded("kevs_attributes_panel");
 
     public static final boolean SPELLPOWER_LOADED = FabricLoader.getInstance().isModLoaded("spell_power");
 
